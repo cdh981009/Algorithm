80c9d73b1612ca937da6a54d7d9770c7801b34bf
Cho Daeho <cdh981009@gmail.com>
2020-02-24 16:59:06 +0900

    Add Problem A

---
+++ div620/A_Two_Rabbits.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR (i, a, b) for (int (i) = (a); (i) < (b); ++(i))
+#define FOR_ (i, a, b) for (int (i) = (a); (i) <= (b); ++(i))
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc; cin >> tc;
+    while (tc--) {
+        int x, y, a, b; cin >> x >> y >> a >> b;
+        int sum = a + b;
+        int delta = y - x;
+        if (delta % sum == 0)
+            cout << delta / sum << "\n";
+        else
+            cout << "-1\n";
+    }
+    return 0;
+}
