5fc2b4816cd2890db7ac25dcf3ed26aca0244459
Cho Daeho <cdh981009@gmail.com>
2020-02-27 17:53:04 +0900

    Create placeholder segment_tree.cpp file.

---
+++ exercise/segment_tree.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); ++(i))
+#define FOR_(i, a, b) for (int (i) = (a); (i) <= (b); ++(i))
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+
+    return 0;
+}
