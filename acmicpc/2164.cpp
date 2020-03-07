032e53fb0048ec324b11ccdb1dddac00bd304924
Cho Daeho <cdh981009@gmail.com>
2020-03-07 17:50:13 +0900

    Solve Problem 2164.

---
+++ acmicpc/2164.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int (i) = (a); (i) < (b); ++(i))
+#define FOR_(i, a, b) for (int (i) = (a); (i) <= (b); ++(i))
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n; cin >> n;
+    deque<int> q; 
+    FOR(i, 0, n) {
+        q.push_front(i + 1);
+    }
+    while (q.size() > 1) {
+        q.pop_back();
+        q.push_front(q.back());
+        q.pop_back();
+    }
+
+    cout << q.back() << endl;
+    return 0;
+}
