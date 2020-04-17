d4e08f5106f02ef47142685765c7fc2583db1cbd
Cho Daeho <cdh981009@gmail.com>
2020-04-17 18:11:47 +0900

    Solve Problem F of div630.

--- div630/F_Independent_Set.cpp
+++ div630/F_Independent_Set.cpp
@@ -10,7 +10,7 @@ int M = 998244353;
 vector<vector<int>> edges(3e5+1, vector<int>());
 int subtree[(int)3e5+1];
 int parent[(int)3e5+1];
-int cache[(int)3e5+1][3][2];
+long long cache[(int)3e5+1][3][2];
  
 void dfs(int node) {
     subtree[node] = 1;
@@ -24,53 +24,55 @@ void dfs(int node) {
 
 enum State { UNPICK, CONNECTED, DISCONNECTED };
 
-int count(int node, State state, bool pick) {
-    int &ref = cache[node][state][pick];
+long long count(int node, State state, bool pick) {
+    long long &ref = cache[node][state][pick];
     if (ref != -1) return ref;
-    int res = 1;
+    long long res = 1;
 
-    // base caswe
+    // base case
     if (subtree[node] == 1) {
-
+        if (state == UNPICK)
+            return ref = 1;
+        if (state == CONNECTED)
+            return ref = 1;
+        if (state == DISCONNECTED)
+            return ref = 0;
     }
 
     if (state == UNPICK) {
         for (auto child : edges[node]) {
             if (child == parent[node]) continue;
-            int subDiscon = count(child, DISCONNECTED, true) + count(child, DISCONNECTED, false);
-            int subUnpick = count(child, UNPICK, false);
+            long long subDiscon = count(child, DISCONNECTED, true) + count(child, DISCONNECTED, false);
+            long long subUnpick = count(child, UNPICK, false);
             subUnpick %= M;
             subDiscon %= M;
-            res *= (subCon + subDiscon) % M;
+            res *= (subDiscon + subUnpick) % M;
             res %= M;
         }
-    }
-
-    for (auto child : edges[node]) {
-        if (child == parent) continue;
-        subCon %= M;
-        subDiscon %= M;
-        res *= (subCon + subDiscon) % M;
-        res %= M;
-    }
-
-    if (!connectToParent) {
-        bool leaf = true;
-        int subDisconProduct = 1;
+    } else {
+        // state DISCONNECTED asserts at least one connection with its child
+        long long minus = 1;
         for (auto child : edges[node]) {
-            if (child == parent) continue;
-            leaf = false;
-            int subDiscon = count(child, node, false, true) + count(child, node, false, false);
+            if (child == parent[node]) continue;
+            long long subCon = (pick ? 0 : count(child, CONNECTED, true)) + count(child, CONNECTED, false);
+            long long subDiscon = count(child, DISCONNECTED, true) + count(child, DISCONNECTED, false);
+            long long subUnpick = count(child, UNPICK, false);
+            // cout << "node " << node << " " << subCon << " " << subDiscon << " " << subUnpick << endl;
+            subCon %= M;
             subDiscon %= M;
-            subDisconProduct *= subDiscon;
-            subDisconProduct %= M;
+            subUnpick %= M;
+            res *= (subCon + subDiscon + subUnpick) % M;
+            res %= M;
+            minus *= (subDiscon + subUnpick) % M;
+            minus %= M;
         }
-        //res = (res - subDisconProduct + M) % M;
+        if (state == DISCONNECTED) 
+            res = (res - minus + M) % M;
     }
-    cout << "node: " << node << " parent: " << parent << " con " << connectToParent << " pick " << pick << " res " << res << endl; 
+    string stateStr[3] = {"unpick", "connected", "disconnected"};
+    //cout << "node: " << node << " parent: " << parent[node] << " state " << stateStr[state]<< " pick " << pick << " res " << res << endl; 
     return ref = res;
 }
-
 
 int main() {
     freopen("input.txt", "r", stdin);
@@ -78,7 +80,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    memset(cache, -1, sizeof(int)*(3e5+1)*4);
+    memset(cache, -1, sizeof(long long)*(3e5+1)*6);
     memset(parent, 0, sizeof(int)*(3e5+1));
 
     int n; cin >> n;
@@ -89,11 +91,12 @@ int main() {
     }
     dfs(1);
 
-    int ans = count(1, UNPICK, false);
+    int ans = count(1, UNPICK, false) - 1 + M;
+    ans %= M;
     ans += count(1, DISCONNECTED, false);
     ans %= M;
     ans += count(1, DISCONNECTED, true);
     ans %= M;
-    cout << ans << endl;
+    cout << ans<< endl;
     return 0;
 }
