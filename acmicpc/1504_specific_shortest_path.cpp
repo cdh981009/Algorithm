aa335747c297cd9912c4c20c2e5fc0afe2bdbe73
Daeho Cho <cdh981009@gmail.com>
2020-07-08 20:04:39 +0900

    Solve Problem 1504: specific shortest path.

--- acmicpc/1504_specific_shortest_path.cpp
+++ acmicpc/1504_specific_shortest_path.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define FOR_(i, a, b) for (int i = (a); i <= (b); ++i)
 
 typedef pair<long long, int> pli;
-int n, m, k;
+int n, m;
 constexpr int N = 800;
 vector<vector<pair<int, int>>> edges(N + 1, vector<pair<int, int>>());
 map<int, vector<long long>> distances;
@@ -16,14 +16,13 @@ void dijkstra(int start) {
     priority_queue<pli, vector<pli>, greater<pli>> q; // pair<distance, vertex>
     vector<long long> dist(n + 1, INF);
     vector<bool> visited(n + 1, false);
-    q.push({start, k});
+    q.push({0, start});
 
     while (!q.empty()) {
         auto top = q.top();
         q.pop();
         auto d = top.first;
         auto node = top.second;
-        //cout << "node " << node << " dist " << d << endl;
         if (visited[node])
             continue;
         dist[node] = d;
@@ -36,7 +35,6 @@ void dijkstra(int start) {
             q.push({d + weight, dst});
         }
     }
-
     distances[start] = std::move(dist);
 }
 
@@ -46,26 +44,23 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> n >> m >> k;
+    cin >> n >> m;
 
     FOR(i, 0, m) {
         int v, u, w;
         cin >> v >> u >> w;
         edges[v].push_back({u, w});
+        edges[u].push_back({v, w});
     }
     int a, b; cin >> a >> b;
-    a--;
-    b--;
 
-    dijkstra(0);
+    dijkstra(1);
     dijkstra(a);
     dijkstra(b);
 
-    long long ans = distances[0][a] + distances[a][b] + distances[b][n-1];
-    ans = min<long long>(ans, distances[0][a] + distances[b][a] + distances[a][n - 1]);
+    long long ans = distances[1][a] + distances[a][b] + distances[b][n];
+    ans = min<long long>(ans, distances[1][b] + distances[b][a] + distances[a][n]);
 
-    cout << distances[0][a] << endl;
-    cout << ans << endl;
     cout << ((ans >= INF) ? -1 : ans) << "\n";
     return 0;
 }
