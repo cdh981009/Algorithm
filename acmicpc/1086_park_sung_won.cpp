cbd323c7d7c2d204b26b2f41c3a9e06bcee33398
Daeho Cho <cdh981009@gmail.com>
2020-07-18 19:56:38 +0900

    still WIP 1086.

--- acmicpc/1086_park_sung_won.cpp
+++ acmicpc/1086_park_sung_won.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define FOR_(i, a, b) for (int i = (a); i <= (b); ++i)
+
 constexpr int N = 15;
 constexpr int K = 100;
+constexpr int L = 50;
 
 int n, k;
-int arr[N];
+string arr[N];
 long long dp[(1 << N)][K];
+int modCache[N];
+int powModCache[K][L + 1];
 
 long long gcd(long long a, long long b) {
 	if (a < b) swap(a, b);
@@ -17,30 +21,21 @@ long long gcd(long long a, long long b) {
 	return gcd(b, a % b);
 }
 
-int digit(long long x) {
-	int cnt = 0;
-	while (x > 0) {
-		x = x/10;
-		cnt++;
-	}
-	return cnt;
-}
-
 long long count(int state, int remainder) {
-	if (state == (1 << (n + 1)) - 1)
-		return remainder == 0;
-	cout << bitset<15>(state) << " " << remainder << endl;
+	//cout << bitset<15>(state) << " " << remainder << endl;
+	if (state == (1 << n) - 1)
+		return remainder == 0 ? 1 : 0;
 	long long& ref = dp[state][remainder];
 	if (ref != -1) return ref;
+	ref = 0;
 	FOR(i, 0, n) {
 		if (state & (1 << i)) continue;
-		long long newRemainder = remainder;
-		FOR(i, 0, digit(arr[i]))
-			newRemainder *= 10;
-		newRemainder += arr[i];
-		newRemainder %= k;
-		cout << i << " " << bitset<15>(state) << " " << newRemainder << endl;
-		ref += count(state | (1 << i), newRemainder);
+		int r = remainder;
+		r *= powModCache[r][arr[i].size()];
+		r += modCache[i];
+		r %= k;
+		// cout << i << " " << bitset<15>(state) << " " << newRemainder << endl;
+		ref += count(state | (1 << i), r);
 	}
 	return ref;
 }
@@ -55,11 +50,34 @@ int main() {
 	cin >> n;
 	long long denom = 1;
 	FOR(i, 0, n) {
-		long long x; cin >> x;
+		string x; cin >> x;
 		arr[i] = x;
 		denom *= (i + 1);
 	}
 	cin >> k;
+
+	FOR(i, 0, n) {
+		string& num = arr[i];
+		int& ref = modCache[i] = 0;
+		FOR(j, 0, num.size()) {
+			ref *= 10;
+			ref += num[j] - '0';
+			ref %= k;
+			//cout << j << " " << ref << endl;;
+		}
+		//cout << num << " " << k << " " << ref << endl; 
+	}
+
+	FOR(i, 0, k) {
+		int r = i;
+		FOR_(j, 1, L) {
+			r *= 10;
+			r %= k;
+			powModCache[i][j] = r;
+			cout << i << " " << j << " " << r << endl;
+		}
+	}
+
 	long long cnt = count(0, 0);
 	long long diviser = gcd(cnt, denom);
 	cout << cnt / diviser << "/" << denom / diviser << "\n";
