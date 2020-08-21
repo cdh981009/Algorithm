7bb9b0065d7d768fb1c52dcfe1de6864e8d7e14b
Daeho Cho <cdh981009@gmail.com>
2020-08-21 19:05:55 +0900

    Solve Problem b, scpc2020.

--- scpc2020/b.cpp
+++ scpc2020/b.cpp
@@ -10,39 +10,14 @@ using namespace std;
 constexpr int N = 3e3;
 int Answer;
 int n, k;
-int dp[N + 1][N + 1];
+bool dp[N + 1][N + 1];
 int a[N + 1];
 int b[N + 1];
-
-bool state(int i, int j) {
-    if (i == 0 && j == 0)
-        return true;
-    int& ref = dp[i][j];
-    if (ref != -1)
-        return ref;
-    ref = false;
-    // 다음 state 하나라도 false면 true
-    // 모두 true면 false
-    int sum = 0;
-    int cnt = 0;
-    while (i > cnt && (sum += a[i - cnt]) <= k) {
-        ++cnt;
-        if (!state(i - cnt, j)) {
-            ref = true;
-            break;
-        }
-    }
-    sum = 0; cnt = 0;
-    while (j > cnt && (sum += b[j - cnt]) <= k) {
-        ++cnt;
-        if (!state(i, j - cnt)) {
-            ref = true;
-            break;
-        }
-    }
-
-    return ref;
-}
+int xLen[N + 1];
+int yLen[N + 1];
+// false count
+int xCnt[N + 1][N + 1];
+int yCnt[N + 1][N + 1];
 
 int main(int argc, char** argv) {
     int T, test_case;
@@ -51,21 +26,63 @@ int main(int argc, char** argv) {
     cin >> T;
     for (test_case = 0; test_case < T; test_case++) {
         Answer = 0;
-        memset(dp, -1, sizeof(int) * (N + 1) * (N + 1));
+        memset(xCnt, 0, sizeof(int) * (N + 1) * (N + 1));
+        memset(yCnt, 0, sizeof(int) * (N + 1) * (N + 1));
         cin >> n >> k;
+        int kLen = 0;
+        int sum = 0;
         for (int i = 0; i < n; ++i) {
             int x;
             cin >> x;
             a[i + 1] = x;
+            sum += x;
+            while (sum > k) {
+                sum -= a[i + 1 - kLen];
+                --kLen;
+            }
+            ++kLen;
+            xLen[i + 1] = kLen;
         }
+        kLen = sum = 0;
         for (int i = 0; i < n; ++i) {
             int x;
             cin >> x;
             b[i + 1] = x;
+            sum += x;
+            while (sum > k) {
+                sum -= b[i + 1 - kLen];
+                --kLen;
+            }
+            ++kLen;
+            yLen[i + 1] = kLen;
         }
-        FOR_(i, 0, n)
-            FOR_(j, 0, n)
-                Answer += state(i, j);
+
+        dp[0][0] = true;
+        ++Answer;
+
+        FOR_(i, 0, n) {
+            FOR_(j, 0, n) {
+                if (i == 0 && j == 0)
+                    continue;
+                auto& ref = dp[i][j] = false;
+                if (i > 0 &&
+                        xCnt[i - 1][j] != (i - 1 - xLen[i] >= 0 ? xCnt[i - 1 - xLen[i]][j] : 0)) {
+                    ref = true;
+                } else if (j > 0 &&
+                           yCnt[i][j - 1] != (j - 1 - yLen[j] >= 0 ? yCnt[i][j - 1 - yLen[j]] : 0)) {
+                    ref = true;
+                }
+                xCnt[i][j] = (i > 0 ? xCnt[i - 1][j] : 0);
+                yCnt[i][j] = (j > 0 ? yCnt[i][j - 1] : 0);
+                if (!ref) {
+                    ++xCnt[i][j];
+                    ++yCnt[i][j];
+                } else {
+                    ++Answer;
+                }
+            }
+        }
+
         cout << "Case #" << test_case + 1 << endl;
         cout << Answer << " " << (n + 1) * (n + 1) - Answer << endl;
     }
