909965285262bf9abb8a490dbdbd2ca3666f0da7
cdh981009 <cdh981009@gmail.com>
2020-09-13 14:47:05 +0900

    Update segment tree code.

--- exercise/segment_tree.cpp
+++ exercise/segment_tree.cpp
@@ -2,65 +2,82 @@
 
 using namespace std;
 
-#define FOR(i, a, b) for (int (i) = (a); (i) < (b); ++(i))
-#define FOR_(i, a, b) for (int (i) = (a); (i) <= (b); ++(i))
-constexpr int N = 1 << 20;
-int n;
-int segTree[N];
+#define INF 987654321
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FOR_(i, a, b) for (int i = (a); i <= (b); ++i)
 
-int sum(int a, int b) {
-    a += n; b += n;
-    int s = 0;
-    while (a <= b) {
-        if (a%2 == 1) s += segTree[a++];
-        if (b%2 == 0) s += segTree[b--];
-        a /= 2; b /= 2;
+constexpr int N = (1 << 21);
+int n;
+// 1-based array
+long long segTree[N];
+
+void initTree(const vector<long long>& arr) {
+    for (n = 1; n < arr.size(); n <<= 1)
+        ;
+    for (int i = 0; i < n; ++i) {
+        segTree[i + n] = (i < arr.size() ? arr[i] : 0 /*default vaule*/);
     }
-    return s;
+    for (int i = n - 1; i >= 1; --i) {
+        segTree[i] = segTree[2 * i] + segTree[2 * i + 1];
+    }
 }
 
-void update(int ind, int val) {
+void pointUpdate(int ind, long long val) {
     ind += n;
     segTree[ind] = val;
     while (ind > 1) {
-        ind /= 2;
-        segTree[ind] = segTree[2*ind] + segTree[2*ind + 1];
+        ind >>= 1;
+        segTree[ind] = segTree[2 * ind] + segTree[2 * ind + 1];
     }
 }
 
-inline int power2RoundUp(int x) {
-    int i;
-    for (i = 1; i < x; i = i << 1);
-    return i;
+long long rangeQuery(int a, int b) {
+    a += n;
+    b += n;
+    long long sum = 0;
+    while (a <= b) {
+        if (a % 2 == 1) // odd
+            sum += segTree[a++];
+        if (b % 2 == 0) // even
+            sum += segTree[b--];
+        a /= 2;
+        b /= 2;
+    }
+    return sum;
 }
 
-void init(const vector<int>& v) {
-    n = power2RoundUp(v.size());
-    cout << "size is: " << n << endl;
-    for (int i = 0; i < n; ++i)
-        segTree[n + i] = (i < v.size() ? v[i] : 0); 
-    for (int i = n - 1; i > 0; --i) 
-        segTree[i] = segTree[2*i] + segTree[2*i + 1];
+void p(int a, int b) {
+    FOR(i, a, b) {
+        cout << segTree[i + n] << " ";
+    }
+    cout << "\n";
 }
 
 int main() {
     freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
+    //freopen("output.txt", "w", stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int len; cin >> len;
-    cout << "len is " << len << endl;
-    vector<int> arr(len);
-    int i = 0;
-    while (len--) {
-        int x; cin >> x;
-        arr[i++] = x;
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<long long> arr(n);
+    FOR(i, 0, n) {
+        long long x;
+        cin >> x;
+        arr[i] = x;
     }
 
-    init(arr);
-    cout << "init complete" << endl;
-    cout << sum(0, 0) << endl;
-    cout << sum(0, 3) << endl;
+    initTree(arr);
+
+    FOR(i, 0, m + k) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) {
+            pointUpdate(b - 1, c);
+        } else {
+            cout << rangeQuery(b - 1, c - 1) << "\n";
+        }
+    }
     return 0;
 }
