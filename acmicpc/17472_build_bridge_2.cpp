c9b6edd2abb14a0a97377882a7682388db4fae00
cdh981009 <cdh981009@gmail.com>
2021-01-13 12:53:17 +0900

    Solve problem 17472: build bridge 2

--- acmicpc/17472_build_bridge_2.cpp
+++ acmicpc/17472_build_bridge_2.cpp
@@ -14,7 +14,7 @@ constexpr int I = 6;
 int n, m;
 int iCnt = 0;
 
-int map[N][N];
+int mp[N][N];
 bool visited[N][N];
 
 int adj[I][I];
@@ -22,91 +22,126 @@ vector<pair<int, pair<int, int>>> edges;
 
 int root[I];
 
+vector<pair<int, int>> islandCells[I];
+
 int find(int x) {
-	return root[x] == x ? x : (root[x] = find(x));
+    return root[x] == x ? x : (root[x] = find(root[x]));
 }
 
 bool same(int x, int y) {
-	return find(x) == find(y);
+    return find(x) == find(y);
 }
 
 void unite(int x, int y) {
-	x = find(x);
-	y = find(y);
-	root[y] = x;
+    x = find(x);
+    y = find(y);
+    root[y] = x;
 }
 
 int kruscal() {
-	FOR(i, 0, iCnt) {
-		FOR(j, i + 1, iCnt) {
-			if (adj[i][j]) {
-				edges.push_back({adj[i][j], {i, j}});
-			}
-		}
-	}
-	
-	FOR(i, 0, iCnt) {
-		root[i] = i;
-	}
-	
-	sort(edges.begin(), edges.end());
-	
-	int ans = 0;
-	int connect = 0;
-	FOR(i, 0, edges.size()) {
-		auto p = edges[i];
-		int x = p.second.first;
-		int y = p.second.second;
-		
-		if (!same(x, y)) {
-			connect++;
-			unite(x, y);
-		}
-	}
-	
-	return (connect != iCnt) ? 0 : ans;
+    FOR(i, 0, iCnt) {
+        FOR(j, i + 1, iCnt) {
+            if (adj[i][j] != INF) {
+                edges.push_back({adj[i][j], {i, j}});
+            }
+        }
+    }
+
+    FOR(i, 0, iCnt) {
+        root[i] = i;
+    }
+
+    sort(edges.begin(), edges.end());
+
+    int ans = 0;
+    int connect = 1;
+    FOR(i, 0, edges.size()) {
+        auto p = edges[i];
+        int x = p.second.first;
+        int y = p.second.second;
+
+        if (!same(x, y)) {
+            connect++;
+            ans += p.first;
+            unite(x, y);
+        }
+    }
+
+    return (connect != iCnt) ? -1 : ans;
 }
 
-vector<pair<int, int>> islandCells[I];
+bool inBound(int x, int y) {
+    return (x >= 0 && y >= 0 && x < n && y < m);
+}
+
+const static pair<int, int> mv[4] = {
+    {0, -1}, {0, +1}, {-1, 0}, {+1, 0}
+};
 
 void dfs(int x, int y) {
-	map[x][y] = iCnt;
-	
-	auto inBound = [](int x, int y) {
-		return (x >= 0 || y >= 0 || x < n || y < m);
-	}
-	
-	const static int move[2] = {-1, +1};
-	FOR(i, 0, 2) {
-		FOR(j, 0, 2) {
-			int nx = x + move[i];
-			int ny = y + move[j];
-			if (!inBound(nx, ny))
-				continue;
-			
-			if (!visited[nx][ny] && map[nx][ny])
-				bfs(nx, ny);
-			
-			if (!map[nx][ny]) // adjacent cell is empty
-				islandCells[iCnt - 1].push_back({x, y});
-		}
-	}
+    mp[x][y] = iCnt;
+	visited[x][y] = true;
+
+    bool put = false;
+    FOR(i, 0, 4) {
+        int nx = x + mv[i].first;
+        int ny = y + mv[i].second;
+        if (!inBound(nx, ny))
+            continue;
+
+        if (!visited[nx][ny] && mp[nx][ny] != 0)
+            dfs(nx, ny);
+
+        if (mp[nx][ny] == 0 && !put) { // adjacent cell is empty
+            islandCells[iCnt - 1].push_back({x, y});
+            put = true;
+        }
+    }
 }
 
 void floodFill() {
-	FOR(i, 0, n) {
-		FOR(j, 0, m) {
-			if (!visited[i][j] && map[i][j]) {
-				dfs(i, j);
-				iCnt++;
-			}
-		}
-	}
+    FOR(i, 0, n) {
+        FOR(j, 0, m) {
+            if (!visited[i][j] && mp[i][j] != 0) {
+                iCnt++;
+                dfs(i, j);
+            }
+        }
+    }
 }
 
 // TODO
 void getAdj() {
-	
+    FOR(i, 0, iCnt) {
+        FOR(j, 0, iCnt) {
+            adj[i][j] = INF;
+        }
+    }
+
+    FOR(i, 0, iCnt) {
+        for (auto& p : islandCells[i]) {
+            FOR(j, 0, 4) {
+                int dirX = mv[j].first;
+                int dirY = mv[j].second;
+
+                int dist = 0;
+                int cx = p.first;
+                int cy = p.second;
+                cx += dirX;
+                cy += dirY;
+                while (inBound(cx, cy) && mp[cx][cy] == 0) {
+                    dist++;
+                    cx += dirX;
+                    cy += dirY;
+                }
+                if (dist >= 2 && inBound(cx, cy) && mp[cx][cy] != 0 && mp[cx][cy] -1 != i) {
+                    int k = mp[cx][cy] - 1;
+                    adj[i][k] = min(adj[i][k], dist);
+                    adj[k][i] = min(adj[k][i], dist);
+                }
+            }
+        }
+    }
 }
 
 int main() {
@@ -115,19 +150,18 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-	cin >> n >> m;
-	FOR(i, 0, n) {
-		FOR(j, 0, m) {
-			int x; cin >> x;
-			map[i][j] = x;
-		}
-	}
-	
-	floodFill();
-	
-	getAdj();
-	
-	cout << kruscal() << '\n';
+    cin >> n >> m;
+    FOR(i, 0, n) {
+        FOR(j, 0, m) {
+            cin >> mp[i][j];
+        }
+    }
+
+    floodFill();
+
+    getAdj();
+
+    cout << kruscal() << '\n';
 
     return 0;
 }
