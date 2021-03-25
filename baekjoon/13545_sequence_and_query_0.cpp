1dd3c5a4ed45ca2fe22c4e673b2c423cf2eaa37d
cdh981009 <cdh981009@gmail.com>
2021-03-25 23:27:27 +0900

    solve problem 13545: sequence and query 0

--- baekjoon/13545_sequence_and_query_0.cpp
+++ baekjoon/13545_sequence_and_query_0.cpp
@@ -8,15 +8,18 @@ using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define FOR_(i, a, b) for (int i = (a); i <= (b); ++i)
 
-constexpr int N = 1e5 + 1;
+constexpr int N = 100010;
 constexpr int SN = 320; // sqrt(N)
 
 int n, m;
 int arr[N];
 int ans[N];
+vector<int> ind[2 * N];
+int top[2 * N];
+int btm[2 * N];
 
 // 환원하면 [L - 1, R]의 범위에서 Ax == Ay인 max(y - x + 1)을 찾는 문제가 된다
-// Mo's algorithm + fenwick tree (max value)
+// Mo's algorithm + sqrt decomposition (max value)
 
 struct Query {
     int l, r, i;
@@ -24,7 +27,60 @@ struct Query {
 
 vector<Query> qs[SN];
 
-int tree[N];
+int cnt[N];
+int bucket[SN];
+
+void addLeft(int i) {
+    int x = arr[i] + N;
+    if (top[x] >= btm[x]) {
+        cnt[ind[x][top[x]] - ind[x][btm[x]]]--;
+        bucket[(ind[x][top[x]] - ind[x][btm[x]]) / SN]--;
+    }
+    btm[x]--;
+    if (top[x] >= btm[x]) {
+        cnt[ind[x][top[x]] - ind[x][btm[x]]]++;
+        bucket[(ind[x][top[x]] - ind[x][btm[x]]) / SN]++;
+    }
+}
+
+void addRight(int i) {
+    int x = arr[i] + N;
+    if (top[x] >= btm[x]) {
+        cnt[ind[x][top[x]] - ind[x][btm[x]]]--;
+        bucket[(ind[x][top[x]] - ind[x][btm[x]]) / SN]--;
+    }
+    top[x]++;
+    if (top[x] >= btm[x]) {
+        cnt[ind[x][top[x]] - ind[x][btm[x]]]++;
+        bucket[(ind[x][top[x]] - ind[x][btm[x]]) / SN]++;
+    }
+}
+
+void removeLeft(int i) {
+    int x = arr[i] + N;
+    if (top[x] >= btm[x]) {
+        cnt[ind[x][top[x]] - ind[x][btm[x]]]--;
+        bucket[(ind[x][top[x]] - ind[x][btm[x]]) / SN]--;
+    }
+    btm[x]++;
+    if (top[x] >= btm[x]) {
+        cnt[ind[x][top[x]] - ind[x][btm[x]]]++;
+        bucket[(ind[x][top[x]] - ind[x][btm[x]]) / SN]++;
+    }
+}
+
+void removeRight(int i) {
+    int x = arr[i] + N;
+    if (top[x] >= btm[x]) {
+        cnt[ind[x][top[x]] - ind[x][btm[x]]]--;
+        bucket[(ind[x][top[x]] - ind[x][btm[x]]) / SN]--;
+    }
+    top[x]--;
+    if (top[x] >= btm[x]) {
+        cnt[ind[x][top[x]] - ind[x][btm[x]]]++;
+        bucket[(ind[x][top[x]] - ind[x][btm[x]]) / SN]++;
+    }
+}
 
 int main() {
     freopen("input.txt", "r", stdin);
@@ -33,9 +89,11 @@ int main() {
     cin.tie(0);
 
     cin >> n;
+    ind[N].push_back(0);
     FOR_(i, 1, n) {
         cin >> arr[i];
         arr[i] += arr[i - 1]; // prefix sum
+        ind[arr[i] + N].push_back(i);
     }
     n++;
 
@@ -48,7 +106,11 @@ int main() {
     }
 
     // Mo's algorithm
+    memset(top, -1, sizeof(int) * 2 * N);
+    top[N] = 0;
     int left = 0, right = 0;
+    cnt[0]++;
+    bucket[0]++;
     for (auto& block : qs) {
         sort(block.begin(), block.end(), [](Query& l, Query& r) { return l.r < r.r; });
         for (auto& q : block) {
@@ -57,19 +119,30 @@ int main() {
             int res;
 
             while (left > l) {
-
+                addLeft(--left);
             }
             while (right < r) {
-
+                addRight(++right);
             }
 
             while (left < l) {
-
+                removeLeft(left++);
             }
             while (right > r) {
-                
+                removeRight(right--);
             }
 
+            for (int i = SN - 1; i >= 0; --i) {
+                if (bucket[i] > 0) {
+                    for (int j = min(N - 1, (i + 1) * SN - 1); j >= i * SN; --j) {
+                        if (cnt[j] > 0) {
+                            res = j;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
             ans[q.i] = res;
         }
     }
