8deaec1f851e1443b7578920e16bed74b3be4d28
cdh981009 <cdh981009@gmail.com>
2021-04-03 18:35:08 +0900

    solve problem 11932: tree and kth number
    use persistent segment tree and tree traversing.
    possibly can reduce runtime by making dynamic allocation automatic.

--- baekjoon/11932_tree_and_kth_number.cpp
+++ baekjoon/11932_tree_and_kth_number.cpp
@@ -8,115 +8,152 @@ using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define FOR_(i, a, b) for (int i = (a); i <= (b); ++i)
 
-const int N = 1010; //10;
+// const int N = 20;
+// const int X = 1 << 10;
 
-// heavy-light decomposition
+const int N = 101010;
+const int LN = 18;
+const int X = 1 << LN;
 
 int n, m;
-vector<int> edges[N];
-int parent[N];
-int subtree[N];
-int depth[N];
+
+// num of leafs in segment tree(power of 2)
+int sn;
 
 int value[N];
+int sorted[N];
+map<int, int> val2Seg;
 
-int segN;
+vector<int> edges[N];
+int par[N][LN];
+int depth[N];
+int sInd[N];
+int eInd[N];
 
-int segHead[N];
-int segIndex[N];
+// persistent segment tree (count tree)
+struct Node {
+    int value = 0;
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
 
-int segCnt = 0;
-vector<int> segArr;
-vector<vector<int>> seg;
+// root node for i-th stage;
+int currStage = 0;
+Node* root[2 * N];
 
-// merge sort tree
-void constructTree() {
-    for (segN = 1; segN <= n; segN <<= 1)
-        ;
-    seg.resize(2 * segN);
-    FOR(i, 0, n) {
-        seg[i + segN].push_back(segArr[i]);
+void buildTree(Node* node, int s, int e) {
+    if (s == e)
+        return;
+    node->left = new Node;
+    node->right = new Node;
+    int mid = (s + e) / 2;
+    buildTree(node->left, s, mid);
+    buildTree(node->right, mid + 1, e);
+}
+
+void pointUpdate(Node* node, Node* old, int nodeLeft, int nodeRight, int ind, int v) {
+    if (nodeLeft == nodeRight) {
+        node->value = v;
+        return;
     }
-    for (int i = segN - 1; i >= 1; --i) {
-        seg[i].resize(seg[2 * i].size() + seg[2 * i + 1].size());
-        merge(seg[2 * i].begin(),     seg[2 * i].end(),
-              seg[2 * i + 1].begin(), seg[2 * i + 1].end(),
-              seg[i].begin());
+
+    int mid = (nodeLeft + nodeRight) / 2;
+
+    if (ind <= mid) {
+        node->left = new Node;
+        node->right = old->right;
+        pointUpdate(node->left, old->left, nodeLeft, mid, ind, v);
+    } else {
+        node->left = old->left;
+        node->right = new Node;
+        pointUpdate(node->right, old->right, mid + 1, nodeRight, ind, v);
     }
+
+    node->value = node->left->value + node->right->value;
+}
+
+void initTree() {
+    for (sn = 1; sn < n; sn <<= 1);
+    // construct default tree
+    root[0] = new Node;
+    buildTree(root[0], 0, sn - 1);
+}
+
+void dfs(int node) {
+    // enter
+    root[++currStage] = new Node;
+    sInd[node] = currStage;
+    pointUpdate(root[currStage], root[currStage - 1], 0, sn - 1, val2Seg[value[node]], 1);
+
+    // traverse children
+    for (auto& child : edges[node]) {
+        if (child == par[node][0])
+            continue;
+        par[child][0] = node;
+        depth[child] = depth[node] + 1;
+        dfs(child);
+    }
+
+    // leave
+    root[++currStage] = new Node;
+    eInd[node] = currStage;
+    pointUpdate(root[currStage], root[currStage - 1], 0, sn - 1, val2Seg[value[node]], 0);
+}
+
+int lca(int x, int y) {
+    if (depth[x] < depth[y])
+        swap(x, y);
+
+    for (int i = LN - 1; i >= 0; --i) {
+        int xx = par[x][i];
+        if (depth[xx] >= depth[y])
+            x = xx;
+    }
+    
+    if (x == y)
+        return x;
+
+    for (int i = LN - 1; i >= 0; --i) {
+        int xx = par[x][i];
+        int yy = par[y][i];
+        if (xx != yy) {
+            x = xx;
+            y = yy;
+        }
+    }
+
+    return par[x][0];
 }
 
 int query(int x, int y, int k) {
-    int res;
+    int l = 0, r = sn - 1;
+    int a = lca(x, y);
+    auto nx = root[sInd[x]];
+    auto ny = root[sInd[y]];
+    auto nz = root[sInd[a]];
+    auto nw = root[sInd[a] - 1];
 
-    vector<vector<int>> vectors;
-    // find lca
-    while (segHead[x] != segHead[y]) {
-        if (depth[segHead[x]] == depth[segHead[y]]) {
-            vectors.push_back()
-            x = parent[segHead[x]];
-            y = parent[segHead[y]];
-        } else if (depth[segHead[x]] > depth[segHead[y]]) {
-            x = parent[segHead[x]];
+    while (l != r) {
+        int mid = (l + r) / 2;
+        int cnt = nx->left->value + ny->left->value
+                - nz->left->value - nw->left->value;
+        if (cnt >= k) {
+            nx = nx->left;
+            ny = ny->left;
+            nz = nz->left;
+            nw = nw->left;
+            r = mid;
         } else {
-            y = parent[segHead[y]];
-        }
-    }
-    // parametric search for kth element
-    // O(log^2(n) * log(1e9))
-    return res = 0;
-}
-
-void hld(int node, int par) {
-    segArr[segCnt++] = node;
-    segIndex[node] = segCnt;
-
-    int maxSize = 0;
-    int maxInd = -1;
-    for (auto& child : edges[node]) {
-        if (child == par)
-            continue;
-        if (maxSize < subtree[child]) {
-            maxSize = subtree[child];
-            maxInd = child;
+            k -= cnt;
+            nx = nx->right;
+            ny = ny->right;
+            nz = nz->right;
+            nw = nw->right;
+            l = mid + 1;
         }
     }
 
-    // primary chain
-    if (maxInd != -1) {
-        segHead[maxInd] = segHead[node];
-        hld(maxInd, node);
-    }
-
-    // branch chain
-    for (auto& child : edges[node]) {
-        if (child == par || child == maxInd)
-            continue;
-        segHead[child] = child;
-        hld(child, node);
-    }
-}
-
-int dfs(int node, int par) {
-    subtree[node] = 1;
-
-    for (auto& child : edges[node]) {
-        if (child == par)
-            continue;
-        parent[child] = node;
-        depth[child] = depth[node] + 1;
-        subtree[node] += dfs(child, node);
-    }
-
-    return subtree[node];
-}
-
-void hld() {
-    dfs(0, 0);
-
-    segArr.resize(N);
-    hld(0, 0);
-
-    constructTree();
+    return sorted[l];
 }
 
 int main() {
@@ -127,25 +164,36 @@ int main() {
 
     cin >> n >> m;
     FOR(i, 0, n) {
-        cin >> value[i];
+        int x; cin >> x;
+        value[i] = x;
+        sorted[i] = x;
+    }
+
+    // compression
+    sort(sorted, sorted + n);
+    FOR(i, 0, n) {
+        val2Seg[sorted[i]] = i;
     }
 
     FOR(i, 0, n - 1) {
-        int a, b;
-        cin >> a >> b;
-        a--;
-        b--;
+        int a, b; cin >> a >> b; a--; b--;
         edges[a].push_back(b);
         edges[b].push_back(a);
     }
 
-    hld();
+    initTree();
+    dfs(0);
+
+    // lca info
+    FOR(i, 1, LN) {
+        FOR(j, 0, n) {
+            par[j][i] = par[par[j][i - 1]][i - 1];
+        }
+    }
 
     FOR(i, 0, m) {
         int x, y, k;
-        cin >> x >> y >> k;
-        x--;
-        y--;
+        cin >> x >> y >> k; x--; y--;
         cout << query(x, y, k) << '\n';
     }
 
