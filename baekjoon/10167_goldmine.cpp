97c69e18aa01c78be305fac669e36b30caf828ad
cdh981009 <cdh981009@gmail.com>
2021-04-21 19:12:29 +0900

    remove lazy propagation in 10167: goldmine

--- baekjoon/10167_goldmine.cpp
+++ baekjoon/10167_goldmine.cpp
@@ -26,46 +26,22 @@ vector<pii> events[N];
 ll mn[X];
 ll mx[X];
 ll nv[X];
-ll lazy[X];
+ll na[X];
 
 void clearTree() {
     memset(mn, 0, sizeof(ll) * X);
     memset(mx, 0, sizeof(ll) * X);
     memset(nv, 0, sizeof(ll) * X);
-    memset(lazy, 0, sizeof(ll) * X);
-}
-
-void propagate(int node, int l, int r) {
-    if (lazy[node] == 0)
-        return;
-
-    ll x = lazy[node];
-    mn[node] += x;
-    mx[node] += x;
-
-    if (l != r) {
-        lazy[2 * node] += x;
-        lazy[2 * node + 1] += x;
-    }
-
-    lazy[node] = 0;
+    memset(na, 0, sizeof(ll) * X);
 }
 
 void update(int s, int e, int v, int node, int l, int r) {
-    propagate(node, l, r);
-
     if (e < l || r < s) {
         return;
     }
 
     if (s <= l && r <= e) {
-        mn[node] += v;
-        mx[node] += v;
-
-        if (l != r) {
-            lazy[2 * node] += v;
-            lazy[2 * node + 1] += v;
-        }
+        na[node] += v;
         return;
     }
 
@@ -73,40 +49,19 @@ void update(int s, int e, int v, int node, int l, int r) {
     update(s, e, v, 2 * node, l, mid);
     update(s, e, v, 2 * node + 1, mid + 1, r);
 
-    mx[node] = max(mx[2 * node], mx[2 * node + 1]);
-    mn[node] = min(mn[2 * node], mn[2 * node + 1]);
+    mx[node] = max(mx[2 * node] + na[2 * node], mx[2 * node + 1] + na[2 * node + 1]);
+    mn[node] = min(mn[2 * node] + na[2 * node], mn[2 * node + 1] + na[2 * node + 1]);
 
     nv[node] = max(nv[2 * node], nv[2 * node + 1]);
-    nv[node] = max(nv[node], mx[2 * node + 1] - mn[2 * node]);
+    nv[node] = max(nv[node], mx[2 * node + 1] + na[2 * node + 1] - mn[2 * node] - na[2 * node]);
 }
 
 void update(int s, int e, int v) {
     update(s, e, v, 1, 0, sn - 1);
 }
 
-ll query(int s, int e, int node, int l, int r) {
-    propagate(node, l, r);
-
-    if (e < l || r < s) {
-        return 0;
-    }
-
-    if (s <= l && r <= e) {
-        return nv[node];
-    }
-
-    int mid = (l + r) / 2;
-    return max(nv[node],
-                    max(query(s, e, 2 * node, l, mid), 
-                        query(s, e, 2 * node + 1, mid + 1, r)));
-}
-
-ll query(int s, int e) {
-    return query(s, e, 1, 0, sn - 1);
-}
-
 int main() {
-    freopen("input.txt", "r", stdin);
+    //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(0);
@@ -152,7 +107,7 @@ int main() {
                 update(p.first, sn - 1, p.second);
             }
 
-            ans = max(ans, query(0, ny - 1));
+            ans = max(ans, nv[1]);
         }
     }
 
