c61a1ecb222f97cd59fb17fe1d7dea149632d5a5
cdh981009 <cdh981009@gmail.com>
2021-09-13 03:59:59 +0900

    upgrade solution D

--- codeforces/global16/D.cpp
+++ codeforces/global16/D.cpp
@@ -11,39 +11,9 @@ using pii = pair<int, int>;
 
 
 constexpr int N = 301;
-int n, m;
-int sn;
+int n, m, k;
 int ans;
-int p[N * N], a[N * N];
-int cnt[N * N];
-int pre[N * N];
-int seg[4 * N * N];
-
-void update(int x, int v) {
-    x += sn;
-    seg[x] += v;
-    while (x > 1) {
-        x /= 2;
-        seg[x] = seg[2 * x] + seg[2 * x + 1];
-    }
-}
-
-int query(int a, int b) {
-    a += sn;
-    b += sn;
-    int ret = 0;
-    while (a <= b) {
-        if (a % 2) {
-            ret += seg[a++];
-        }
-        if (b % 2 == 0) {
-            ret += seg[b--];
-        }
-        a /= 2;
-        b /= 2;
-    }
-    return ret;
-}
+pii p[N];
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -58,38 +28,25 @@ int main() {
 
     while (tc--) {
         cin >> n >> m;
-        for (sn = 1; sn < n * m; sn <<= 1)
-            ;
-        memset(seg, 0, sizeof(int) * 2 * sn);
-        memset(pre, 0, sizeof(int) * n * m);
-        memset(cnt, 0, sizeof(int) * n * m);
-
-        FOR(i, 0, n * m) {
-            cin >> p[i];
-            a[i] = p[i];
-        }
-
-        sort(p, p + n * m);
-
-        int up = unique(p, p + n * m) - p;
-        FOR(i, 0, n * m) {
-            a[i] = lower_bound(p, p + up, a[i]) - p;
-            pre[a[i]]++;
-            cnt[a[i]] = 0;
-        }
-
-        FOR(i, 1, n * m) {
-            pre[i] += pre[i - 1];
-        }
-
         ans = 0;
-        FOR(i, 0, n * m) {
-            int x = a[i] == 0 ? 0 : pre[a[i] - 1];
-            x += cnt[a[i]]++;
-            int r = x / m;
+        k = n * m;
+        FOR(i, 0, k) {
+            cin >> p[i].first;
+            p[i].second = i;
+        }
+        sort(p, p + k);
+        FOR(i, 0, k) {
+            p[i].second = -p[i].second;
+        }
 
-            ans += query(r * m, (a[i] == 0 ? 0 : pre[a[i] - 1]) -1);
-            update(x, 1);
+        FOR(i, 0, n) {
+            sort(p + i * m, p + (i + 1) * m);
+            FOR(j, 0, m) {
+                FOR(k, j + 1, m) {
+                    if (p[i * m + j].second > p[i * m + k].second)
+                        ans++;
+                }
+            }
         }
 
         cout << ans << '\n';
