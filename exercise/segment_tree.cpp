305e3850414ac7b5395a4410861c8fc0d1e0bd07
cdh981009 <cdh981009@gmail.com>
2021-09-13 03:09:36 +0900

    update segtree code

--- exercise/segment_tree.cpp
+++ exercise/segment_tree.cpp
@@ -6,51 +6,47 @@ using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define FOR_(i, a, b) for (int i = (a); i <= (b); ++i)
 
-constexpr int N = (1 << 21);
-int n;
+using ll = long long;
+
+constexpr int N = 101010;
+
+int n, sn;
 // 1-based array
-long long segTree[N];
+ll seg[4 * N];
 
-void initTree(const vector<long long>& arr) {
-    for (n = 1; n < arr.size(); n <<= 1)
+void init(const vector<ll>& arr) {
+    for (sn = 1; sn < n; sn <<= 1)
         ;
-    for (int i = 0; i < n; ++i) {
-        segTree[i + n] = (i < arr.size() ? arr[i] : 0 /*default vaule*/);
+    for (int i = 0; i < sn; ++i) {
+        seg[i + sn] = (i < arr.size() ? arr[i] : 0);
     }
-    for (int i = n - 1; i >= 1; --i) {
-        segTree[i] = segTree[2 * i] + segTree[2 * i + 1];
-    }
-}
-
-void pointUpdate(int ind, long long val) {
-    ind += n;
-    segTree[ind] = val;
-    while (ind > 1) {
-        ind >>= 1;
-        segTree[ind] = segTree[2 * ind] + segTree[2 * ind + 1];
+    for (int i = sn - 1; i >= 1; --i) {
+        seg[i] = seg[2 * i] + seg[2 * i + 1];
     }
 }
 
-long long rangeQuery(int a, int b) {
-    a += n;
-    b += n;
-    long long sum = 0;
+void update(int x, ll v) {
+    x += n;
+    seg[x] = v;
+    while (x > 1) {
+        x /= 2;
+        seg[x] = seg[2 * x] + seg[2 * x + 1];
+    }
+}
+
+ll query(int a, int b) {
+    a += sn;
+    b += sn;
+    ll sum = 0;
     while (a <= b) {
         if (a % 2 == 1) // odd
-            sum += segTree[a++];
+            sum += seg[a++];
         if (b % 2 == 0) // even
-            sum += segTree[b--];
+            sum += seg[b--];
         a /= 2;
         b /= 2;
     }
     return sum;
-}
-
-void p(int a, int b) {
-    FOR(i, a, b) {
-        cout << segTree[i + n] << " ";
-    }
-    cout << "\n";
 }
 
 int main() {
@@ -61,22 +57,22 @@ int main() {
 
     int n, m, k;
     cin >> n >> m >> k;
-    vector<long long> arr(n);
+    vector<ll> arr(n);
     FOR(i, 0, n) {
-        long long x;
+        ll x;
         cin >> x;
         arr[i] = x;
     }
 
-    initTree(arr);
+    init(arr);
 
     FOR(i, 0, m + k) {
-        long long a, b, c;
+        ll a, b, c;
         cin >> a >> b >> c;
         if (a == 1) {
-            pointUpdate(b - 1, c);
+            update(b - 1, c);
         } else {
-            cout << rangeQuery(b - 1, c - 1) << "\n";
+            cout << query(b - 1, c - 1) << "\n";
         }
     }
     return 0;
