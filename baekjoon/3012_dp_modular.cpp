fba0cae6a1992ed36601600f6f1a076e9362d3ba
cdh981009 <cdh981009@gmail.com>
2021-09-20 00:20:32 +0900

    update solution

--- baekjoon/3012_dp_modular.cpp
+++ baekjoon/3012_dp_modular.cpp
@@ -14,13 +14,11 @@ using ll = long long;
 
 int n;
 ll dp[N][N];
-bool of[N][N];
-string str;
+char str[N];
 
-bool getOf(int s, int e) {
-    if (s > e)
-        return false;
-    return of[s][e];
+ll mod(ll x) {
+    if (x >= M) return M + x % M;
+    else return x;
 }
 
 ll getDp(int s, int e) {
@@ -53,18 +51,11 @@ ll getDp(int s, int e) {
     for (int i = s + 1; i <= e; i += 2) {
         char ec = str[i];
         if (match(sc, ec)) {
-            ll x = getDp(s + 1, i - 1);
-            ll y = getDp(i + 1, e);
+            ll x = getDp(s + 1, i - 1) * getDp(i + 1, e);
             if (sc == '?' && ec == '?')
                 x *= 3;
-            if ((!getOf(s + 1, i - 1) && x == 0) || (!getOf(i + 1, e) && y == 0))
-                continue;
-            if (getOf(s + 1, i - 1) || getOf(i + 1, e))
-                of[s][e] = true;
-            ref += x * y;
-            if (ref >= M)
-                of[s][e] = true;
-            ref %= M;
+            ref += x;
+            ref = mod(ref);
         }
     }
 
@@ -72,25 +63,18 @@ ll getDp(int s, int e) {
 }
 
 void solve() {
-    cin >> n >> str;
+    scanf("%d%s", &n, str);
     if (n % 2 == 1) {
-        cout << 0 << '\n';
+        printf("%d\n", 0);
         return;
     }
-    memset(dp, -1, sizeof(ll) * N * N);
+    memset(dp, -1, sizeof(dp));
     ll ans = getDp(0, n - 1);
 
-    if (of[0][n - 1]) {
-        string s;
-        s.resize(5);
-        FOR(i, 0, 5) {
-            int x = ans % 10;
-            ans /= 10;
-            s[4 - i] = x + '0';
-        }
-        cout << s << '\n';
+    if (ans >= M) {
+        printf("%05lld\n", ans - M);
     } else {
-        cout << ans << '\n';
+        printf("%d\n", ans);
     }
 }
 
@@ -99,10 +83,6 @@ int main() {
     freopen("input.txt", "r", stdin);
 //freopen("output.txt", "w", stdout);
 #endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
     solve();
-
     return 0;
 }
