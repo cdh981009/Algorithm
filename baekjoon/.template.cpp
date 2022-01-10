44ad3dbabaf6e333aab2457747691b7339a52871
cdh981009 <cdh981009@gmail.com>
2022-01-10 22:36:47 +0900

    add template

---
+++ baekjoon/.template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF 987654321
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FOR_(i, a, b) for (int i = (a); i <= (b); ++i)
+
+using ll = long long;
+
+constexpr int N = 1e5;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    return 0;
+}
