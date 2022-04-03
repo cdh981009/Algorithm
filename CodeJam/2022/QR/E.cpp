795bd4094635e5ca31261eef92b2ab806f496b9f
cdh981009 <cdh981009@gmail.com>
2022-04-03 18:11:41 +0900

    solve problem E

--- CodeJam/2022/QR/E.cpp
+++ CodeJam/2022/QR/E.cpp
@@ -20,7 +20,7 @@ using vpll = vector<pii>;
 using mii = map<int, int>;
 using mll = map<ll, ll>;
 
-constexpr int N = 2001;
+constexpr int N = 101010;
 constexpr int M = 100;
 
 ll n, k, m, ans;
@@ -32,7 +32,47 @@ int main() {
     int tc;
     cin >> tc;
     rep_(testcase, 1, tc) {
+        srand(time(NULL));
+
         cin >> n >> k;
+        vl ps(n+1, -1);
+        ll r, p;
+        ll sum = 0, ans = 0;
+        cin >> r >> p;
+        ps[r] = p;
+        sum += p;
+        
+        rep(i, 0, k) {
+            if (i % 2 == 0) {
+                int room = (rand() % n + 1);
+                cout << "T " << room << '\n';
+                cout.flush();
+                cin >> r >> p;
+                ps[r] = p;
+                sum += p;
+            } else {
+                cout << "W\n";
+                cout.flush();
+                cin >> r >> p;
+                ps[r] = p;
+            }
+        }
+
+        ll cnt = 0;
+        ll avg = sum / (k/2);
+        sum = 0;
+        rep_(i, 1, n) {
+            if (ps[i] == -1) cnt++;
+            else sum += ps[i];
+        }
+
+        ans += avg * cnt;
+        ans += sum;
+        
+
+        cout << "E " << ans/2 << '\n';
+        cout.flush();
+
     }
 
     return 0;
