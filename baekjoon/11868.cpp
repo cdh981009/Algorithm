1fd2ea0c8981f451c47ee051ad1c43998504fa05
cdh981009 <cdh981009@gmail.com>
2022-08-19 02:28:33 +0900

    Solve nim game

---
+++ baekjoon/11868.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF (1e9 + 7)
+#define fi first
+#define se second
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep_(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FOR_(i, a, b) for (int i = (a); i <= (b); ++i)
+#define printYN(pos) cout << (pos ? "YES" : "NO") << '\n'
+#define printPOS(pos) cout << (pos ? "POSSIBLE" : "IMPOSSIBLE") << '\n'
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+
+constexpr int N = 110;
+constexpr int M = 8e6 + 10;
+constexpr int B = 4e6;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    //  freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n; cin >> n;
+    int x, s;
+    s = 0;
+    rep(i, 0, n) {
+        cin >> x;
+        s ^= x;
+    }
+    if (s == 0) cout << "cubelover\n";
+    else cout << "koosaga\n";
+    
+    return 0;
+}
